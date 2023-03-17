0aaecc0ccb75bf4b59b2cfacfb58df1e8a179351
Jones-Kofi <amanorjoneskofi@gmail.com>
2023-03-17 01:39:42 +0000

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-/** 
- * main - Entry
+/**
+ * main - this is the main function
  *
- * Description: 'grammer'
- *
- * Return: Always 0 (Success)
+ * Return: 0 when successful
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
