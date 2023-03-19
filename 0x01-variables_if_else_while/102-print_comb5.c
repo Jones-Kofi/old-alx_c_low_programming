34055aca8c726e537aac0457b0fb108839c8c2d3
Jones-Kofi <amanorjoneskofi@gmail.com>
2023-03-19 22:08:56 +0000

    Print combo 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 9 ; j++)
+		{
+			for (k = 0 ; k < 10 ; k++)
+			{
+				for (l = 1 ; l < 10 ; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i + j + k + l != 35)
+					{
+						putchar(',');
+					}
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
