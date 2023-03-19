1788e5c4dceecc5ad066843d83e84673aadd8d4e
Jones-Kofi <amanorjoneskofi@gmail.com>
2023-03-19 22:45:02 +0000

    combination commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 9 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
 			for (k = 0 ; k < 10 ; k++)
 			{
-				for (l = 1 ; l < 10 ; l++)
+				for (l = 0 ; l < 10 ; l++)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
